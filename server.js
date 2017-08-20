7ba69913d3727dddda919df42eb61f8928feb99f
pacific415 <30789675+pacific415@users.noreply.github.com>
2017-08-20 08:50:19 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.sendFile(path.join(__dirname, 'ui','index.html'));
 });
 
 app.get('/article1',function(req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'article1.html')); 
+   res.sendFile(path.join(__dirname, 'ui','article1.html')); 
 });
 
 app.get('/article2',function(req,res){
