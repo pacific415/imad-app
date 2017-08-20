77913e4eea39b51cdf04e25c8cf2cd860a6a3759
pacific415 <30789675+pacific415@users.noreply.github.com>
2017-08-20 08:47:10 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,9 +12,11 @@ app.get('/', function (req, res) {
 app.get('/article1',function(req,res){
    res.sendFile(path.join(__dirname, 'ui', 'article1.html')); 
 });
+
 app.get('/article2',function(req,res){
    res.send('Article 2 requested and will be served here') 
 });
+
 app.get('/article3',function(req,res){
    res.send('Article 3 requested and will be served here') 
 });
