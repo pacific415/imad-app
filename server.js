2979c47ccb2bb4e3d24a899c0d547a50da848c83
pacific415 <30789675+pacific415@users.noreply.github.com>
2017-08-21 02:11:28 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,16 +22,15 @@ var articleOne = {
  };
  
 functionn createTemplate(date){ 
-
-        var htmlTemplate =`
+var htmlTemplate =`
            <html>
-          `  <head>
+           `<head>`
                 <title>
-                  ${title}
-                </title>
+                    ${title}
+               </title>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
                 <link href="/ui/style.css" rel="stylesheet" />
-             </head>
+            </head>
             <body>
                 <div class="container">
                     <div>
