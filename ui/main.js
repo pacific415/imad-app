261887cade05e9cae47ec57f787a376594b918c5
pacific415 <30789675+pacific415@users.noreply.github.com>
2017-09-01 07:20:38 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,7 @@
 console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHtml = 'New value';
+var img = document.getElementById('img');
+img.onclick = function () {
+  img.style.marginLeft = '100px';  
+};
