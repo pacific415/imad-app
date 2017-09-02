eb321bc14e612e80e339cf272fd66296bb067e5e
pacific415 <30789675+pacific415@users.noreply.github.com>
2017-09-02 01:55:38 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,9 +3,9 @@ var button = document.getElementById('counter');
 button.onclick = function(){
 //create request
     var request = new XMLHttpRequest();
-    request.onreadystatechange = function(){
+    request.onreadystatechange = function () {
         if (requst.readyState === XMLHttpRequest.DONE){
-            if (request.status === 200){
+            if (request.status === 200) {
                 var counter = request.responseText;
                var span = document.getElementById('count');
              span.innerHTML = counter.toString();
