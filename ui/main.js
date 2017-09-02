89e46438aed6f28b05acd82ba84185d8fefaac6c
pacific415 <30789675+pacific415@users.noreply.github.com>
2017-09-02 01:59:42 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,7 +13,7 @@ button.onclick = function(){
         }
     };
 
-request.open('GET', 'http://prashantjadhav415.imad.hasura-app.io/counter', true);
-request.send(null);
+   request.open('GET', 'http://prashantjadhav415.imad.hasura-app.io/counter', true);
+   request.send(null);
 
 };
