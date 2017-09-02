37d48c2c4a0c1f5f32c2d2a292114c831c5f47c7
pacific415 <30789675+pacific415@users.noreply.github.com>
2017-09-02 01:39:32 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,5 +2,5 @@ var button = document.getElementById('counter');
 button.onclick = function(){
     counter = counter + 1;
     var span = document.getElementById('count');
-    span.iinerHTML = counter.toString();
+    span.innerHTML = counter.toString();
 };
